3c5fbdc7b2b322e24682a3ea4320d1a1d533f316
Dawid <dawid@batou.localdomain>
2026-05-04 19:58:55 +0200

    completely reorganized

---
+++ install_hyprland.sh
@@ -0,0 +1,17 @@
+# install basic hyprland packages
+
+sudo pacman -Sy hyprland alacritty pipewire wireplumber xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper hyprlock
+
+# install notifications deamon
+
+sudo pacman -Sy mako
+
+# wayland qt-support
+
+sudo pacman -Sy qt5-wayland qt6-wayland
+
+# additional fonts
+
+sudo pacman -Sy noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
+
+
